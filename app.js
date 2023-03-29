2b6074f05290e6516e13bf0d28c9c23817ea31d6
yaroslavMain <122780614+yaroslavMain@users.noreply.github.com>
2023-03-29 21:35:44 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+let btn = document.getElementById("btn");
+
+btn.addEventListener("click", function()  {
+	    tg.sendData([{
+		"name": document.getElementById("name").value,
+		"emai": document.getElementById("email").value,
+		"tel": document.getElementById("tel").value,
+		"password": document.getElementById("password").value
+	}]);
+});
