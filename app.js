3ccb1ec75f9969d24875fa974a4e43e473df8e36
yaroslavMain <122780614+yaroslavMain@users.noreply.github.com>
2023-03-29 21:56:18 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ let btn = document.getElementById("btn");
 
 btn.addEventListener("click", function()  {
 	    tg.sendData([{
-		"name": document.getElementById("name").value,
 		"emai": document.getElementById("email").value,
-		"tel": document.getElementById("tel").value,
 		"password": document.getElementById("password").value
 	}]);
 });
